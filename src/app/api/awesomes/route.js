ed4fd7b98ac274ca5d2acb5d716162e356838c23
Ryan Valle <ryanvalle@me.com>
2023-05-11 23:22:13 -0700

    Initial Commit

---
+++ src/app/api/awesomes/route.js
@@ -0,0 +1,57 @@
+import { NextResponse } from 'next/server';
+const { Client } = require("@notionhq/client")
+ 
+export async function POST(req) {
+    const notion = new Client({
+        auth: process.env.NOTION_TOKEN
+    });
+    // bb24dbc2cdf74a7d8792a7ed7153c08e
+    const { domain } = await req.json();
+
+    if (!domain) {
+        return NextResponse.json({ error: 'Bad Request' });
+    }
+
+    const response = await notion.databases.query({
+        database_id: "bb24dbc2cdf74a7d8792a7ed7153c08e",
+        filter: {
+
+            "and": [
+                {
+                    "property": "domain-match",
+                    "title": {
+                        "equals": domain
+                    }
+                },
+                {
+                    "property": "enabled",
+                    "checkbox": {
+                        "equals": true
+                    }
+                }
+            ]
+        }
+    }).then((data) => {
+        try {
+            let properties = data.results[0]['properties'];
+            return {
+                url: properties['redirect-url']['url'],
+                text: {
+                    h1: properties['h1-text']['rich_text'][0]['plain_text'],
+                    h3: properties['h3-text']['rich_text'][0]['plain_text'],
+                    linkText: properties['link-text']['rich_text'][0]['plain_text']
+                }
+            }
+        } catch {
+            return {
+                text: {
+                    h1: 'Hello World'
+                }
+            }
+        }
+    });
+
+    const data = response;
+ 
+    return NextResponse.json({ ...data });
+}
---
+++ src/app/components/funtext.js
@@ -0,0 +1,67 @@
+'use client';
+ 
+import { useEffect, useState } from 'react';
+import Image from 'next/image'
+
+import { Oswald, Roboto } from 'next/font/google'
+import Link from 'next/link';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100','300', '500']
+})
+
+const oswald = Oswald({
+  subsets: ['latin'],
+  weight: ['400','700']
+})
+ 
+export default function Funtext() {
+    const [h1Text, setH1] = useState();
+    const [h3Text, setH3] = useState();
+    const [url, setURL] = useState();
+    const [linktext, setLinkText] = useState();
+    const [isHidden, setHidden] = useState(true);
+    useEffect(() => {
+        fetch('/api/awesomes', {
+            method: 'POST',
+            body: JSON.stringify({domain: location.host}),
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+              }
+        })
+        .then((response) => response.json())
+        .then((resp) => {
+            setH1(resp.text.h1);
+            setH3(resp.text.h3);
+            setURL(resp.url);
+            setLinkText(resp.text.linkText);
+            setTimeout(() => {
+                setHidden(false)
+            }, 500)
+        });
+    })
+ 
+  return (
+    <>
+      <div className={`absolute w-screen h-screen flex items-center align-center ${!isHidden ? 'opacity-0 scale-100' : 'opacity-1 scale-125'} transition-all duration-1000`}>
+        <Image 
+          src="/loader.svg"
+          width={300}
+          height={300}
+          className='w-screen'
+        />
+      </div>
+      <div className={`text-center w-screen ${isHidden ? 'opacity-0 scale-100' : 'opacity-1 scale-125'} transition-all duration-1000`}>
+        <h1 className={`uppercase font-bold ${oswald.className} text-6xl text-black text-shadow transform-matrix`}>{h1Text}</h1>
+        <h3 className={`${roboto.className} uppercase font-bold text-black text-xl pt-[10px] transform-matrix pb-[40px]`}>{h3Text}</h3>
+        {url && linktext && <Link
+            href={url} 
+            target="_blank"
+            className={`custom-button ${oswald.className} text-black px-[20px] py-[10px] mt-[30px] rounded-md`}>
+                {linktext}</Link>}
+      </div>
+    </>
+  );
+}
