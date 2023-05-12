d8b7fc7da6eade4dc14ba668fcfed4aa21585bee
Ryan Valle <ryanvalle@me.com>
2023-05-12 00:06:41 -0700

    Title update

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'kindofaweso.me',
+  title: '...is kind of awesome',
   description: 'Kind Of Awesome',
 }
 
